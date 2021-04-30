4e6bcdb665c77c92407a486539d67b497ec2b89e
Andy Thomas <andycthomas@gmail.com>
2021-04-30 19:03:52 +0100

    day16 code

---
+++ day16-start/main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+timmy = Turtle()
+print(timmy)
+
+my_screen = Screen()
+timmy.shape('turtle')
+timmy.color('green')
+timmy.forward(100)
+print(my_screen.canvheight)
+my_screen.exitonclick()
