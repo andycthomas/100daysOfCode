3f7a10578ba785881285b5a77f18dea4d6efb815
Andy Thomas <andycthomas@gmail.com>
2021-05-01 14:56:05 +0100

    Work in progress for day 17 quiz game

--- day-17/main.py
+++ day-17/main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -7,4 +8,8 @@ for q in question_data:
     question = Question(q['text'], q['answer'])
     question_bank.append(question)
 
-)
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
---
+++ day-17/quiz_brain.py
@@ -0,0 +1,16 @@
+class QuizBrain:
+    def __init__(self, question_list):
+        self.question_number = 0
+        self.question_list = question_list
+
+    def next_question (self):
+        current_question = self.question_list [self.question_number].text
+        self.question_number += 1
+        input(f'Q.{self.question_number}: {current_question} (True/False): ')
+
+    def still_has_questions(self):
+        if self.question_number < len(self.question_list):
+            return True
+        else:
+            return False
+
