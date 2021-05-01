633f93148d78c628da99d7bf417f744eb89e9058
Andy Thomas <andycthomas@gmail.com>
2021-05-01 11:21:37 +0100

    Initial version of oo coffee machine

--- day-16-coffee-machine/main.py
+++ day-16-coffee-machine/main.py
@@ -1,3 +1,19 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+makingCoffee = True
+
+coffeeMaker = CoffeeMaker ()
+menu = Menu()
+
+while makingCoffee is True:
+    request = input("What would you like ? " + menu.get_items() + "\n")
+
+    if request == "off":
+        makingCoffee = False
+        print("Turning off coffee machine")
+    elif request == "report":
+        coffeeMaker.report()
+    else:
+        print("Do something!")
