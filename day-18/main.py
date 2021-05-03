9e135583031553b6a7ee64e5ead9645710866db0
Andy Thomas <andycthomas@gmail.com>
2021-05-03 19:02:07 +0100

    Drawing polygons with Turtle graphics

--- day-18/main.py
+++ day-18/main.py
@@ -1,15 +1,25 @@
 from turtle import Turtle, Screen
+import random
 
 tim = Turtle()
 
 
 tim.shape('turtle')
-tim.color('limeGreen')
 
-for _ in range (0,4):
-    tim.forward(100)
-    tim.right(90)
+colours = ['CornflowerBlue', 'DarkOrchid', 'IndianRed', 'DeepSkyBLue', 'LightSeaGreen', 'wheat', 'SlateGray', 'SeaGreen']
 
+def draw_shape(edges):
+    angle = 360.0 / edges
+    print (angle)
+
+    for _ in range (1, edges + 1 ):
+        tim.forward(100)
+        tim.right(angle)
+
+
+for _ in range(3,11):
+    tim.color(random.choice(colours))
+    draw_shape(_)
 
 screen = Screen()
 screen.exitonclick()
